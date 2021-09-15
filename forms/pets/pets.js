3953eb8733a0fc226405b84348f62fe81cbcb4f6
Maura Small <maurasmall@creighton.edu>
2021-09-15 14:38:06 -0500

    LA Week 4

---
+++ forms/pets/pets.js
@@ -0,0 +1,9 @@
+let  petNames = ['Darcy' ,  'Jack' , 'Jerry' , 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam' ];
+
+let withSal = [petNames.push('Sal')];
+
+console.log(`The third pet is named ${petNames[2]}`);
+
+console.log(`The array has ${withSal.length} names in it.` );
+
+withSal.pop();
